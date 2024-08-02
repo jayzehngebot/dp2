ab32a064b7cc74a313dc171c8bc49c1cfb2ffa12
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-08-02 08:55:29 -0400

    saving. most basic setup running

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Import the framework and instantiate it
 import Fastify from 'fastify'
 import NodeCache from 'node-cache'
 import * as dotenv from 'dotenv';
@@ -8,8 +7,8 @@ import fastifyView from '@fastify/view';
 import handlebars from 'handlebars';
 import fastifyStatic from '@fastify/static';
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+const __filename = fileURLToPath(import.meta.url); // GET RESOLVED PATH
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -22,26 +21,24 @@ fastify.register(fastifyView, {
     handlebars: handlebars
   },
   root: path.join(__dirname, 'views'),
-//  layout: 'layout', // optional: if you use a layout template
-  viewExt: 'hbs'    // default file extension for templates
+  viewExt: 'hbs'    // HANDLEBARS EXT
 });
 
-// Serve static files from the /public directory
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, 'public'),
-  prefix: '/public/', // optional: default is '/'
+  prefix: '/public/',
 });
-
 
 const dataCache = new NodeCache();
 
-// Declare a route
+// ROUTING
 fastify.get('/', async function handler (request, reply) {
 
   let podData = dataCache.get( "sheetsData" );
 
   if ( podData == undefined ){
-      console.log("dataUndefined - fetch data");
+
+      // console.log("data Undefined - fetch data");
 
       let apiKey = process.env.GOOGLE_SHEETS_API_KEY;
       let sheetName = process.env.SHEET_NAME;
@@ -57,18 +54,37 @@ fastify.get('/', async function handler (request, reply) {
         console.error('Error:', error);
       }
 
-      dataCache.set( "sheetsData", freshData, 10000 );
-      console.log('success setting sheetsData');
+      dataCache.set( "sheetsData", freshData, 60 );
       podData = dataCache.get( "sheetsData" );
-
   }
 
-  //  reply.type('text/html');
-  //  reply.send('hello : '+ JSON.stringify(podData));
-  return reply.view('/index', { title: 'Home', data: podData });
+  const [headers, ...rows] = podData;
+
+  // DEFINE KEY MAPPINGS
+  const keyMappings = {
+    Podcast: "podcast",
+    Episode: "episodeURL",
+    DateRange: "dateRange",
+    Comments: "comments"
+  };
+  
+  // TRANSFORM DATA
+  const transformData = (row) => {
+    return row.reduce((acc, value, index) => {
+      const key = headers[index];
+      acc[keyMappings[key]] = value;
+      return acc;
+    }, {});
+  };
+  
+  const transformedData = rows.map(transformData);
+  
+  // console.log(transformedData);
+  // reply.send(podData);
+  return reply.view('/index', { title: 'Home', data: transformedData });
 })
 
-// Run the server!
+// RUN SERVER
 try {
   await fastify.listen({ port: 3000 })
 } catch (err) {
